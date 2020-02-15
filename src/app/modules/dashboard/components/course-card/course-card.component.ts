dd46eba7390150fa9d437db6406ed6aad4657a62
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-15 19:50:41 +0100

    Refactor CourseCard component

--- src/app/modules/dashboard/components/course-card/course-card.component.ts
+++ src/app/modules/dashboard/components/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CourseWithStudents } from './../../../../models/course.model';
 
 @Component({
   selector: 'app-course-card',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-card.component.scss']
 })
 export class CourseCardComponent implements OnInit {
+  @Input() course: CourseWithStudents;
 
   constructor() { }
 
