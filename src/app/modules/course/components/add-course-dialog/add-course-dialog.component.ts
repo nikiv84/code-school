b424f65310a05194052bc0f7560881dcc11fd0ec
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 19:15:40 +0100

    Refactor AddCourseDialog component

--- src/app/modules/course/components/add-course-dialog/add-course-dialog.component.ts
+++ src/app/modules/course/components/add-course-dialog/add-course-dialog.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-course-dialog',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCourseDialogComponent implements OnInit {
 
-  constructor() { }
+  public nameFormControl = new FormControl('', [Validators.required]);
+  public dateFormControl = new FormControl('', [Validators.required]);
+
+  public addCourseForm: FormGroup = new FormGroup({
+    name: this.nameFormControl,
+    date: this.dateFormControl
+  });
+
+  constructor(
+    public dialogRef: MatDialogRef<AddCourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data) {
+  }
 
   ngOnInit(): void {
   }
 
+  public onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  public onYesClick(): void {
+    if (this.addCourseForm.valid && this.addCourseForm.dirty) {
+      const course = {
+        name: this.nameFormControl.value,
+        date: this.dateFormControl.value
+      };
+      this.dialogRef.close(course);
+    }
+  }
 }
