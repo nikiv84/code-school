473a0e14d1d3ccd335b97dfc777a099af913863a
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 01:50:59 +0100

    Add imports to Material module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
-    MatIconModule
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   exports: [
-    MatIconModule
+    MatIconModule,
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
