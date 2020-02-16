b3724631660e6915a552d323316d2b23ad29793e
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 01:50:42 +0100

    Refactor Course component

--- src/app/modules/course/components/course/course.component.ts
+++ src/app/modules/course/components/course/course.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { CourseStudent } from './../../../../models/course-student.model';
+import { Course } from './../../../../models/course.model';
+import { Student } from './../../../../models/student.model';
+import { CourseService } from './../../services/course.service';
+import { DialogComponent } from './../dialog/dialog.component';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +15,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  private courseId: string;
+  public students$: Observable<Student[]>;
+  public course: Course;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private courseService: CourseService,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.courseId = this.route.snapshot.paramMap.get('courseId');
+    this.students$ = this.getCourseData();
+  }
+
+  private getCourseData(): Observable<Student[]> {
+    return this.courseService.getCourse(this.courseId)
+      .pipe(
+        switchMap((course: Course) => {
+          this.course = course;
+          return this.courseService.getEnrollmentForCourse(this.courseId)
+            .pipe(
+              switchMap((enrollments: CourseStudent[]) =>
+                forkJoin(enrollments.map((enrollment: CourseStudent) => this.courseService.getStudent(enrollment.studentId))))
+            );
+        })
+      );
+  }
+
+  public removeStudentFromCourse(studentId: string) {
+    this.students$ = this.courseService.removeStudentFromCourse(this.courseId, studentId)
+      .pipe(
+        switchMap(() => this.getCourseData())
+      );
+  }
+
+  public openDialog(student: Student): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '360px',
+      data: {
+        student
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((shouldRemove: boolean) => {
+      if (shouldRemove) {
+        this.removeStudentFromCourse(student.id);
+      }
+    });
   }
 
 }
