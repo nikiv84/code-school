58c3aaf80bb27085f8215c38b430010f2da1535f
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 06:53:55 +0100

    Add imports to material module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
@@ -10,13 +13,19 @@ import { MatTableModule } from '@angular/material/table';
     MatIconModule,
     MatTableModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   exports: [
     MatIconModule,
     MatTableModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatFormFieldModule
   ]
 })
 export class MaterialModule { }
