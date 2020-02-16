b43de5fc0b0d7203c93508ac66a77e8ce3180feb
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 06:53:39 +0100

    Add loadingSpinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { MessageComponent } from './components/message/message.component';
 import { MaterialModule } from './material.module';
 
 @NgModule({
+  declarations: [
+    LoadingSpinnerComponent,
     MessageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FormsModule
   ],
   exports: [
+    MaterialModule,
+    LoadingSpinnerComponent,
     MessageComponent,
     FormsModule
   ]
