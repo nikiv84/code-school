c7ab0f5dcc258c00142e8ba1f19690dd0067d6c6
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 14:51:34 +0100

    Remove Dialog component

--- src/app/modules/student/student.module.ts
+++ src/app/modules/student/student.module.ts
@@ -2,17 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from './../../shared/shared.module';
 import { AddStudentDialogComponent } from './components/add-student-dialog/add-student-dialog.component';
-import { DialogComponent } from './components/dialog/dialog.component';
+import { EditStudentDialogComponent } from './components/edit-student-dialog/edit-student-dialog.component';
 import { StudentTableComponent } from './components/student-table/student-table.component';
 import { StudentComponent } from './components/student/student.component';
 import { StudentRoutingModule } from './student-routing.module';
-import { EditStudentDialogComponent } from './components/edit-student-dialog/edit-student-dialog.component';
 
 @NgModule({
   declarations: [
     StudentComponent,
     StudentTableComponent,
-    DialogComponent,
     AddStudentDialogComponent,
     EditStudentDialogComponent
   ],
@@ -23,7 +21,6 @@ import { EditStudentDialogComponent } from './components/edit-student-dialog/edi
   ],
   entryComponents: [
     AddStudentDialogComponent,
-    DialogComponent
   ]
 })
 export class StudentModule { }
