0263380adf1b9bf4ade710627c9843c58f69e275
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 02:00:07 +0100

    Add Student component

--- src/app/modules/student/student.module.ts
+++ src/app/modules/student/student.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { StudentComponent } from './components/student/student.component';
 import { StudentRoutingModule } from './student-routing.module';
 
-
 @NgModule({
-  declarations: [],
+  declarations: [
+    StudentComponent
+  ],
   imports: [
     CommonModule,
     StudentRoutingModule
