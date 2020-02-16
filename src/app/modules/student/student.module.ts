9f1dce51afd243145b9e5bf1d8e91df61c30efec
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 02:02:42 +0100

    Add StudentTable component

--- src/app/modules/student/student.module.ts
+++ src/app/modules/student/student.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { StudentComponent } from './components/student/student.component';
 import { StudentRoutingModule } from './student-routing.module';
+import { StudentTableComponent } from './components/student-table/student-table.component';
 
 @NgModule({
   declarations: [
-    StudentComponent
+    StudentComponent,
+    StudentTableComponent
   ],
   imports: [
     CommonModule,
