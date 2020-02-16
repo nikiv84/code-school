26c34bd5718357c5932243c728cad5484b1e7f0e
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 22:33:50 +0100

    Add NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 import { SharedModule } from './shared/shared.module';
+import { CourseEffects } from './store/effects';
+import { reducer } from './store/reducer';
 
 @NgModule({
   declarations: [
@@ -21,7 +26,12 @@ import { SharedModule } from './shared/shared.module';
     HttpClientModule,
     BrowserAnimationsModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forRoot({ courses: reducer }),
+    EffectsModule.forRoot([CourseEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 15
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
