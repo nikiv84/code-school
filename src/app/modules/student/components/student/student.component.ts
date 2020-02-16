ea4a48437e7c286c5256a3c864f50155f048e1f1
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 02:24:04 +0100

    Refactor Student Component

--- src/app/modules/student/components/student/student.component.ts
+++ src/app/modules/student/components/student/student.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { StudentWithCourses } from 'src/app/models/student.model';
+import { Student } from './../../../../models/student.model';
+import { StudentService } from './../../services/student.service';
 
 @Component({
   selector: 'app-student',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
+  public students$: Observable<StudentWithCourses[]>;
 
-  constructor() { }
+  constructor(private studentService: StudentService) { }
 
   ngOnInit(): void {
+    this.students$ = this.getAllStudents();
+  }
+
+  public getAllStudents(): Observable<StudentWithCourses[]> {
+    return this.studentService.getAllStudents()
+      .pipe(
+        switchMap((students: Student[]) =>
+          forkJoin(students.map((student: Student) => this.studentService.getCourseCountForStudent(student.id)))
+            .pipe(
+              map((numberOfCourses: number[]) => students.map((student: Student, index: number) =>
+                new StudentWithCourses(student.id, student.firstName,
+                  student.lastName, student.email, numberOfCourses[index], student.phone)))
+            )
+        )
+      );
   }
 
 }
