3715df1a7895809b3953e6f9cfda95652f748731
Ivan Nikolic <inikolic84po@gmail.com>
2020-02-16 13:57:51 +0100

    Refactor EditStudentDialog component

--- src/app/modules/student/components/edit-student-dialog/edit-student-dialog.component.ts
+++ src/app/modules/student/components/edit-student-dialog/edit-student-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface DialogData {
@@ -15,11 +16,32 @@ export interface DialogData {
 })
 export class EditStudentDialogComponent implements OnInit {
 
+  public emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  public firstnameFormControl = new FormControl('', [Validators.required]);
+  public lastnameFormControl = new FormControl('', [Validators.required]);
+  public phoneFormControl = new FormControl('');
+
+  public editStudentForm: FormGroup = new FormGroup({
+    email: this.emailFormControl,
+    firstName: this.firstnameFormControl,
+    lastName: this.lastnameFormControl,
+    phone: this.phoneFormControl
+  });
+
   constructor(
     public dialogRef: MatDialogRef<EditStudentDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  }
 
   ngOnInit(): void {
+    this.setFormValues();
+  }
+
+  setFormValues() {
+    this.editStudentForm.controls['firstName'].setValue(this.data.firstName);
+    this.editStudentForm.controls['lastName'].setValue(this.data.lastName);
+    this.editStudentForm.controls['email'].setValue(this.data.email);
+    this.editStudentForm.controls['phone'].setValue(this.data.phone);
   }
 
   public onNoClick(): void {
@@ -27,7 +49,15 @@ export class EditStudentDialogComponent implements OnInit {
   }
 
   public onYesClick(): void {
-    this.dialogRef.close(this.data);
+    if (this.editStudentForm.valid && this.editStudentForm.dirty) {
+      const student = {
+        firstName: this.firstnameFormControl.value,
+        lastName: this.lastnameFormControl.value,
+        email: this.emailFormControl.value,
+        phone: this.phoneFormControl.value
+      };
+      this.dialogRef.close(student);
+    }
   }
 
 }
